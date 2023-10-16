818cc15c252a4be556816b05ee6e4e13bf43029e
skyketip <skyketip@gmail.com>
2023-10-16 17:09:23 +0300

    Adding project to f

---
+++ nina.py
@@ -0,0 +1,23 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+code =int(input('room code: '))
+req = requests.get('https://api.quizit.online/quizizz/answers', params={'pin':code})
+req1 = json.loads(req.text)
+print(req)
+print(req1['message'])
+data = req1["data"]
+answers = data["answers"]
+question_count =len(answers)
+dick ={}
+for i in range(0,question_count):
+	print(str(i) +".")
+	dick["count"+str(i)]=answers[i]
+	a = dict(dick["count"+str(i)])
+	b =a['answers']
+	c=a['question']
+	d =c['text']
+	print("question: " +str(d[d.find('<p>')+3 : d.find('</p>')]))
+	print("answer: "+ str(a['answers']))
+	print("type: "+str(a['type']))
+print("number of questions: "+str(question_count))
